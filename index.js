56c099d23077e43e5d26cd316ecb303783db4093
unknown <samuli.ahlqvist@helsinki.fi>
2018-02-19 17:48:58 +0200

    tehtävät 1-2

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+//laita urliin user ja pass
+const mongoUrl = 'mongodb://user:pass@ds243008.mlab.com:43008/fullstack-blog'
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
